aecb3ae6f9097b7a02c5aa7e6f84a03443480a59
Santy121655 <santylopes62@gmail.com>
2024-03-11 13:54:02 -0600

    capitulo 9

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+  };
+
+export  default function Page() {
+    return (
+        <div className="w-full">
+          <div className="flex w-full items-center justify-between">
+            <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+          </div>
+        </div>
+      );
+    }
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,65 +1,61 @@
-import { generateYAxis } from '@/app/lib/utils';
-import { CalendarIcon } from '@heroicons/react/24/outline';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
-
-// This component is representational only.
-// For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
-// https://airbnb.io/visx/
-
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  const chartHeight = 350;
-  // NOTE: comment in this code when you get to this point in the course
-
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
-   if (!revenue || revenue.length === 0) {
-     return <p className="mt-4 text-gray-400">No data available.</p>;
-   }
-
+import { fetchLatestInvoices } from '@/app/lib/data';
+export default async function LatestInvoices() { // Remove props
+  const latestInvoices = await fetchLatestInvoices();
   return (
-    <div className="w-full md:col-span-4">
+    <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Recent Revenue
+        Latest Invoices
       </h2>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+        {/* NOTE: comment in this code when you get to this point in the course */}
 
-      { <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
-          >
-            {yAxisLabels.map((label) => (
-              <p key={label}>{label}</p>
-            ))}
-          </div>
-
-          {revenue.map((month) => (
-            <div key={month.month} className="flex flex-col items-center gap-2">
+       <div className="bg-white px-6">
+          {latestInvoices.map((invoice, i) => {
+            return (
               <div
-                className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
-              ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-                {month.month}
-              </p>
-            </div>
-          ))}
-        </div>
+                key={invoice.id}
+                className={clsx(
+                  'flex flex-row items-center justify-between py-4',
+                  {
+                    'border-t': i !== 0,
+                  },
+                )}
+              >
+                <div className="flex items-center">
+                  <Image
+                    src={invoice.image_url}
+                    alt={`${invoice.name}'s profile picture`}
+                    className="mr-4 rounded-full"
+                    width={32}
+                    height={32}
+                  />
+                  <div className="min-w-0">
+                    <p className="truncate text-sm font-semibold md:text-base">
+                      {invoice.name}
+                    </p>
+                    <p className="hidden text-sm text-gray-500 sm:block">
+                      {invoice.email}
+                    </p>
+                  </div>
+                </div>
+                <p
+                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
+                >
+                  {invoice.amount}
+                </p>
+              </div>
+            );
+          })}
+        </div> 
         <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
-      </div> }
+      </div>
     </div>
   );
 }
